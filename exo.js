aca9f4635715d4869894311aaa7ffabd1975acb0
Olivier02500 <olivier.peuchet@hotmail.com>
2021-11-25 15:28:47 +0100

    make exo 82

---
+++ exo.js
@@ -0,0 +1,32 @@
+let floor = 34.589456 ;
+let floor2 = 68.412784 ;
+let ceil = 39.456 ;
+let ceil2 = 27.34876541 ;
+let round = 5.68 ;
+let round2 = 5.41 ;
+let trunc = 6.12457 ;
+let trunc2 = 9.568124 ;
+//min
+let a = 5, b = 1, c = 12,d = -1, e = 45;
+let f = 9, i = 12, g= 16,k = 44,l = 3;
+//max
+let m = 5,n = 1,o = 12, p= -1,q = 45;
+let  r = 9,s = 12, t = 16,u = 44,v = 3;
+let abs = -6;
+let abs2 = -56;
+
+console.log( "1 = " + Math.floor(floor));
+console.log( "2 = " + Math.floor(floor2));
+console.log( "3 = " + Math.ceil(ceil));
+console.log( "4 = " + Math.ceil(ceil2));
+console.log( "5 = " + Math.round(round));
+console.log( "6 = " + Math.round(round2));
+console.log( "7 = " + Math.trunc(trunc));
+console.log( "8 = " + Math.trunc(trunc2));
+console.log( "9 = " + Math.min(a ,b ,c ,d ,e));
+console.log( "10 = " + Math.min(f ,i ,g ,k ,l));
+console.log( "11 = " + Math.max(m ,n ,o ,p ,q));
+console.log( "12 = " + Math.max(r ,s ,t ,u ,v));
+console.log( "13 = " + Math.abs(abs));
+console.log( "14 = " + Math.abs(abs2));
+
